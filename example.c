7b32643a5a5cc0b2a8950e2fbb3aa085cf1eeced
Paul Rouget <me@paulrouget.com>
2017-09-16 11:33:12 +0200

    update

--- example.c
+++ example.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
+void* callback(char* name) {
+  return 0;
+}
+
 int main(void) {
   void* h = dlopen("./target/debug/libsimpleservo.dylib", RTLD_LOCAL);
   if (h) {
-    char* (*version)() = dlsym(h, "servo_version");
-    if (version) {
-      printf("servo version: %s\n", version());
+    void (*init)(void *(char *)) = dlsym(h, "init");
+    if (init) {
+      init(callback);
     } else {
       printf("Can't find symbol\n");
     }
@@ -15,5 +19,7 @@ int main(void) {
   }
   if (dlclose(h) != 0) {
     printf("Unable to close library: %s", dlerror());
+  }
+  while(1) {
   }
 }
